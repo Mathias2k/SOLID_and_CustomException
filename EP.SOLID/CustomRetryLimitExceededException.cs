a124c30bb45d256fd02f3db6bb35659ce93e46ae
Mathias2k <rodolfo_psl@hotmail.com>
2022-08-27 13:47:41 -0300

    all 5 principles of solid by Eduardo Pires and Custom Exception from book Troelsen A. Pro C# 10 with .NET 6...Programming 11ed 2022

---
+++ EP.SOLID/CustomRetryLimitExceededException.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EP.SOLID
+{
+    public class CustomRetryLimitExceededException : CustomException
+    {
+        public CustomRetryLimitExceededException() { }
+        public CustomRetryLimitExceededException(string message)
+        : base(message) { }
+        public CustomRetryLimitExceededException(
+        string message, RetryLimitExceededException innerException)
+        : base(message, innerException) { }
+    }
+}
